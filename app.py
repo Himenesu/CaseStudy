1c8e3fdcb0f5ede35aa5bb770816d24a77cfcd95
Jhervin Jimenez <80193756+Himenesu@users.noreply.github.com>
2024-12-10 05:51:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 # Load models
 models = {
-    'model1': joblib.load('model1.pkl'),
-    'model2': joblib.load('model2.pkl'),
-    'model3': joblib.load('model3.pkl')
+    'lr_model': joblib.load('lr_model.pkl'),
+    'mlp': joblib.load('mlp.pkl'),
+    'stacking_model': joblib.load('stacking_model.pkl')
 }
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -20,7 +19,7 @@ def predict():
     input_data = data.get('inputs')
 
     if model_choice not in models:
-        return jsonify({'error': 'Model not found. Choose from model1, model2, model3.'}), 400
+        return jsonify({'error': 'Model not found. Choose from Linear Model, Neural Model, Stacking Model.'}), 400
 
     # Process the input data
     processed_data = np.array(input_data).reshape(1, -1)
